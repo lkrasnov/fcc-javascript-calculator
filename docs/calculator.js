e25b5a601cf1dc1670f79ef2af4bbdef693290fd
Leo Krasnov <leoboy254@gmail.com>
2018-01-21 11:46:53 -0600

    added handlers for each button

--- docs/calculator.js
+++ docs/calculator.js
@@ -1,3 +1,85 @@
+var result = 0;
+
 $(document).ready(function() {
     
 });
+
+$("#zero").click(function() {
+    (this).blur();
+});
+
+$("#one").click(function() {
+    (this).blur();
+});
+
+$("#two").click(function() {
+    (this).blur();
+});
+
+$("#three").click(function() {
+    (this).blur();
+});
+
+$("#four").click(function() {
+    (this).blur();
+});
+
+$("#five").click(function() {
+    (this).blur();
+});
+
+$("#six").click(function() {
+    (this).blur();
+});
+
+$("#seven").click(function() {
+    (this).blur();
+});
+
+$("#eight").click(function() {
+    (this).blur();
+});
+
+$("#nine").click(function() {
+    (this).blur();
+});
+
+$("#dec").click(function() {
+    (this).blur();
+});
+
+$("#eq").click(function() {
+    (this).blur();
+});
+
+$("#plus").click(function() {
+    (this).blur();
+});
+
+$("#minus").click(function() {
+    (this).blur();
+});
+
+$("#mult").click(function() {
+    (this).blur();
+});
+
+$("#div").click(function() {
+    (this).blur();
+});
+
+$("#per").click(function() {
+    (this).blur();
+});
+
+$("#ac").click(function() {
+    (this).blur();
+});
+
+$("#ce").click(function() {
+    (this).blur();
+});
+
+function updateDisplay() {
+    
+}
